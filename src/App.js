84cfbc169e386e52cadb462a2d5def0a3beeb8fa
edwardSmite <edsmite2002@gmail.com>
2021-08-04 13:23:42 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react"
+import Board from "./components/Board"
+import "./style.css"
+
+function App() {
+    const [num, setNum] = useState(0)
+    const [num1, setNum1] = useState(0)
+
+    function handleChange(e) {
+        e.preventDefault()
+        setNum(e.target.value)
+    }
+
+    function handleChange1(e) {
+        e.preventDefault()
+        setNum1(num)
+        document.getElementById("aa").style.display = "none"
+    }
+    return (
+        <div>
+            <form id="aa" onSubmit={handleChange1}>
+                <input type="text" onChange={handleChange}/>
+                <input type="submit" value="submit"/>
+                <br/>
+            </form>
+        <div className="game">
+            <Board n={num1}/>
+        </div>
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Board.js
@@ -0,0 +1,35 @@
+import React from "react"
+import Square from "./Square"
+
+export default function Board(props) {
+    function renderSquare(i) {
+        return (
+            <Square 
+                value=""
+                onClick={() => console.log(i)}
+                key={i}
+            />
+        )
+    }
+
+    var b = []
+    for (let i = 0; i < props.n; i++) {
+        var c = []
+        for (let j = 0; j < props.n; j++) {
+            c.push(
+                renderSquare(i * props.n + j)
+            )
+        }
+        b.push(
+            <div className="board-row" key={i}>
+                {c}
+            </div>
+        )
+    }
+    return (
+        <div>
+            {b}
+        </div>
+    )
+}
+
