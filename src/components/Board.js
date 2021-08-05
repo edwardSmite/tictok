2a64f73c4d98cf1aa531dfa7dce1bc189321b835
yahuy1 <nguyengiahuyak29@gmail.com>
2021-08-05 15:51:03 +0700

    update result calculator

--- src/components/Board.js
+++ src/components/Board.js
@@ -21,37 +21,34 @@ export default function Board(props) {
 
             function checkPath(id) {
                 var satisfied = false
-                var i = id / props.n
-                var j = id % props.n
-                const add = [-1, 0, 1]
+                const dx = [0, 1, 1, 1, 0, -1, -1, -1];
+                const dy = [-1, 0, 1, -1, 1, 0, -1, 1];
+                let count = [0, 0, 0, 0, 0, 0, 0, 0];
 
-                for (let a in add) {
-                    for (let b in add) {
-                        if (a * b !== 0) {
-                            if (squares[(i + a) * props.n + (j + b)] === squares[id]) {
-                                if (squares[(i - a) * props.n + (j - b)] !== squares[id]) {
-                                    if (squares[(i + 2 * a) * props.n + (j + 2 * b)] === squares[id]) {
-                                        if (squares[(i + 3 * a) * props.n + (j + 3 * b)] === squares[id]) {
-                                            if (squares[(i + 4 * a) * props.n + (j + 4 * b)] === squares[id]) {
-                                                satisfied = true
-                                            } else {break}
-                                        } else {break}
-                                    } else {break}
-                                } else {
-                                    if (squares[(i + 2 * a) * props.n + (j + 2 * b)] === squares[id] && squares[(i - 2 * a) * props.n + (j - 2 * b)] === squares[id]) {
-                                        satisfied = true
-                                    } else if (squares[(i + 2 * a) * props.n + (j + 2 * b)] === squares[id] && squares[(i + 3 * a) * props.n + (j + 3 * b)] === squares[id]) {
-                                        satisfied = true
-                                    } else if (squares[(i - 2 * a) * props.n + (j - 2 * b)] === squares[id] && squares[(i - 3 * a) * props.n + (j - 3 * b)] === squares[id]) {
-                                        satisfied = true
-                                    } else {break}
-                                }
-                            } else {break}
-                        } 
+                for (let k = 0; k < 8; k++) {
+                    let newRow = Math.floor(id / props.n);
+                    let newColumn = id % props.n;
+                    while (count[k] < 5) {
+                        newRow = newRow + dx[k];
+                        newColumn = newColumn + dy[k];
+                        let newId = newRow * props.n + newColumn;
+
+                        if (newRow < 0 || newColumn < 0 || newRow >= props.n || newColumn >= props.n || squares[newId] !== squares[id]) {
+                            break;
+                        } else {
+                            count[k]++;
+                        }
+                    }
+                }                
+                
+                for (let k = 0; k < 4; k++) {
+                    if (count[k] + count[k + 4] >= 4) {
+                        satisfied = true;
+                        break;
                     }
                 }
+
                 setWin(satisfied)
-                console.log(win)
             }
             checkPath(id)
         }
@@ -80,11 +77,12 @@ export default function Board(props) {
         )
     }
 
-    var indi = !win ? <div><h1 id="aa">Current: {xTurn ? "X" : "O"}</h1><br/><br/></div> : <div><h1>Winner: {xTurn ? "X" : "O"}</h1></div>
+    var indi = !win ? <h1 id="aa">Current: {xTurn ? "X" : "O"}</h1> : <h1>Winner: {!xTurn ? "X" : "O"}</h1>;
 
     return (
         <div>
             {indi}
+            <br/><br/>
             <div className="board">
                 {b}
             </div>
