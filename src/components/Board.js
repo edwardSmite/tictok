2d8cd76e40356f811535bbb6bab497eebe47733c
edwardSmite <edsmite2002@gmail.com>
2021-08-05 15:25:46 +0700

    j

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,6 +4,7 @@ import Square from "./Square"
 export default function Board(props) {
     const [xTurn, setXTurn] = useState(true)
     const [squares, setSquares] = useState([])
+    const [win, setWin] = useState(false)
 
     useEffect(() => {
         setSquares([])
@@ -17,11 +18,50 @@ export default function Board(props) {
                 newSquares[id] = xTurn ? "X" : "O"
             } 
             setSquares([...newSquares])
+
+            function checkPath(id) {
+                var satisfied = false
+                var i = id / props.n
+                var j = id % props.n
+                const add = [-1, 0, 1]
+
+                for (let a in add) {
+                    for (let b in add) {
+                        if (a * b !== 0) {
+                            if (squares[(i + a) * props.n + (j + b)] === squares[id]) {
+                                if (squares[(i - a) * props.n + (j - b)] !== squares[id]) {
+                                    if (squares[(i + 2 * a) * props.n + (j + 2 * b)] === squares[id]) {
+                                        if (squares[(i + 3 * a) * props.n + (j + 3 * b)] === squares[id]) {
+                                            if (squares[(i + 4 * a) * props.n + (j + 4 * b)] === squares[id]) {
+                                                satisfied = true
+                                            } else {break}
+                                        } else {break}
+                                    } else {break}
+                                } else {
+                                    if (squares[(i + 2 * a) * props.n + (j + 2 * b)] === squares[id] && squares[(i - 2 * a) * props.n + (j - 2 * b)] === squares[id]) {
+                                        satisfied = true
+                                    } else if (squares[(i + 2 * a) * props.n + (j + 2 * b)] === squares[id] && squares[(i + 3 * a) * props.n + (j + 3 * b)] === squares[id]) {
+                                        satisfied = true
+                                    } else if (squares[(i - 2 * a) * props.n + (j - 2 * b)] === squares[id] && squares[(i - 3 * a) * props.n + (j - 3 * b)] === squares[id]) {
+                                        satisfied = true
+                                    } else {break}
+                                }
+                            } else {break}
+                        } 
+                    }
+                }
+                setWin(satisfied)
+                console.log(win)
+            }
+            checkPath(id)
         }
+
         return (
             <Square 
                 value={squares[i]}
-                onClick={() => {sthing(i)}}
+                onClick={() => {
+                    sthing(i)
+                }}
                 key={i}
             />
         )
@@ -39,10 +79,12 @@ export default function Board(props) {
             <div className="board-row" key={i}>{c}</div>
         )
     }
-    
+
+    var indi = !win ? <div><h1 id="aa">Current: {xTurn ? "X" : "O"}</h1><br/><br/></div> : <div><h1>Winner: {xTurn ? "X" : "O"}</h1></div>
+
     return (
         <div>
-            <h1 id="aa">Current: {xTurn ? "X" : "O"}</h1><br/><br/>
+            {indi}
             <div className="board">
                 {b}
             </div>
--- src/components/CheckWin.js
+++ src/components/CheckWin.js
@@ -6,6 +6,5 @@ export default function CheckWin(n) {
 
     useEffect(() => {}, xTurn)
 
-
     return isEnded
 }
